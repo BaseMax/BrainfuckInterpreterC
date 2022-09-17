ac606a0f7ef13848f6beb6a882a2bb9ebbd85efe
Max Base <maxbasecode@gmail.com>
2022-09-17 21:38:41 +0200

    fix bug of brainfuck interpreter

--- brainfuck.c
+++ brainfuck.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* read_file(char* file_path)
 {
@@ -23,61 +24,139 @@ char* read_file(char* file_path)
 
 char* interpret(char* bf)
 {
-    char* data = malloc(30000);
-    char* ptr = data;
-    char* output = malloc(30000);
-    char* out_ptr = output;
+    // >	Move the pointer to the right
+    // <	Move the pointer to the left
+    // +	Increment the memory cell at the pointer
+    // -	Decrement the memory cell at the pointer
+    // .	Output the character signified by the cell at the pointer
+    // ,	Input a character and store it in the cell at the pointer
+    // [	Jump past the matching ] if the cell at the pointer is 0
+    // ]	Jump back to the matching [ if the cell at the pointer is nonzero
+    // All characters other than ><+-.,[] should be considered comments and ignored. But, see extensions below.
 
-    while (*bf != '\0') {
-        switch (*bf) {
-            case '>':
-                ptr++;
-                break;
-            case '<':
-                ptr--;
-                break;
-            case '+':
-                (*ptr)++;
-                break;
-            case '-':
-                (*ptr)--;
-                break;
-            case '.':
-                *out_ptr = *ptr;
-                out_ptr++;
-                break;
-            case ',':
-                *ptr = getchar();
-                break;
-            case '[':
-                if (*ptr == 0) {
-                    int depth = 1;
-                    while (depth > 0) {
-                        bf++;
-                        if (*bf == '[') {
-                            depth++;
-                        } else if (*bf == ']') {
-                            depth--;
-                        }
-                    }
-                }
-                break;
-            case ']':
-                if (*ptr != 0) {
-                    int depth = 1;
-                    while (depth > 0) {
-                        bf--;
-                        if (*bf == ']') {
-                            depth++;
-                        } else if (*bf == '[') {
-                            depth--;
-                        }
-                    }
-                }
-                break;
+    char* output = malloc(30000);
+    // initialize memory
+    int memory[30000];
+    // initialize pointer
+    int pointer = 0;
+    // initialize output
+    int output_pointer = 0;
+
+    // loop through input
+    // i = 0
+    // while i < len(input):
+    //     # print("i: {}, input[i]: {}, memory: {}, pointer: {}".format(i, input[i], memory, pointer))
+    //     # >	Move the pointer to the right
+    //     if input[i] == ">":
+    //         pointer += 1
+    //     # <	Move the pointer to the left
+    //     elif input[i] == "<":
+    //         pointer -= 1
+    //     # +	Increment the memory cell at the pointer
+    //     elif input[i] == "+":
+    //         memory[pointer] += 1
+    //     # -	Decrement the memory cell at the pointer
+    //     elif input[i] == "-":
+    //         memory[pointer] -= 1
+    //     # .	Output the character signified by the cell at the pointer
+    //     elif input[i] == ".":
+    //         output += chr(memory[pointer])
+    //     # ,	Input a character and store it in the cell at the pointer
+    //     elif input[i] == ",":
+    //         memory[pointer] = ord(input())
+    //     # [	Jump past the matching ] if the cell at the pointer is 0
+    //     elif input[i] == "[":
+    //         if memory[pointer] == 0:
+    //             # find matching ]
+    //             count = 1
+    //             while count > 0:
+    //                 i += 1
+    //                 if input[i] == "[":
+    //                     count += 1
+    //                 elif input[i] == "]":
+    //                     count -= 1
+    //     # ]	Jump back to the matching [ if the cell at the pointer is nonzero
+    //     elif input[i] == "]":
+    //         if memory[pointer] != 0:
+    //             # find matching [
+    //             count = 1
+    //             while count > 0:
+    //                 i -= 1
+    //                 if input[i] == "]":
+    //                     count += 1
+    //                 elif input[i] == "[":
+    //                     count -= 1
+    //     else:
+    //         pass
+    //     i += 1
+
+    int i = 0;
+    while (i < strlen(bf)) {
+        // >	Move the pointer to the right
+        if (bf[i] == '>') {
+            pointer += 1;
         }
-        bf++;
+        // <	Move the pointer to the left
+        else if (bf[i] == '<') {
+            pointer -= 1;
+        }
+        // +	Increment the memory cell at the pointer
+        else if (bf[i] == '+') {
+            memory[pointer] += 1;
+        }
+        // -	Decrement the memory cell at the pointer
+        else if (bf[i] == '-') {
+            memory[pointer] -= 1;
+        }
+        // .	Output the character signified by the cell at the pointer
+        else if (bf[i] == '.') {
+            output[output_pointer] = memory[pointer];
+            output_pointer += 1;
+        }
+        // ,	Input a character and store it in the cell at the pointer
+        else if (bf[i] == ',') {
+            memory[pointer] = getchar();
+        }
+        // [	Jump past the matching ] if the cell at the pointer is 0
+        else if (bf[i] == '[') {
+            if (memory[pointer] == 0) {
+                // find matching ]
+                int count = 1;
+                while (count > 0) {
+                    i += 1;
+                    if (bf[i] == '[') {
+                        count += 1;
+                    }
+                    else if (bf[i] == ']') {
+                        count -= 1;
+                    }
+                }
+            }
+        }
+        // ]	Jump back to the matching [ if the cell at the pointer is nonzero
+        else if (bf[i] == ']') {
+            if (memory[pointer] != 0) {
+                // find matching [
+                int count = 1;
+                while (count > 0) {
+                    i -= 1;
+                    if (bf[i] == ']') {
+                        count += 1;
+                    }
+                    else if (bf[i] == '[') {
+                        count -= 1;
+                    }
+                }
+            }
+        }
+        else {
+            // pass
+        }
+        i += 1;
     }
+
+    // add \0 to output
+    output[output_pointer] = '\0';
 
     return output;
 }
