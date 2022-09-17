6f112e1e17287eef43282d883bbaa651ce5b0853
Max Base <maxbasecode@gmail.com>
2022-09-17 21:31:47 +0200

    complete interpreter

--- brainfuck.c
+++ brainfuck.c
@@ -1,7 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+char* read_file(char* file_path)
+{
+    char* data = NULL;
+    FILE* file = fopen(file_path, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    data = malloc(file_size + 1);
+    fread(data, file_size, 1, file);
+    data[file_size] = '\0';
+
+    fclose(file);
+    return data;
+}
+
+char* interpret(char* bf)
+{
+    char* data = malloc(30000);
+    char* ptr = data;
+    char* output = malloc(30000);
+    char* out_ptr = output;
+
+    while (*bf != '\0') {
+        switch (*bf) {
+            case '>':
+                ptr++;
+                break;
+            case '<':
+                ptr--;
+                break;
+            case '+':
+                (*ptr)++;
+                break;
+            case '-':
+                (*ptr)--;
+                break;
+            case '.':
+                *out_ptr = *ptr;
+                out_ptr++;
+                break;
+            case ',':
+                *ptr = getchar();
+                break;
+            case '[':
+                if (*ptr == 0) {
+                    int depth = 1;
+                    while (depth > 0) {
+                        bf++;
+                        if (*bf == '[') {
+                            depth++;
+                        } else if (*bf == ']') {
+                            depth--;
+                        }
+                    }
+                }
+                break;
+            case ']':
+                if (*ptr != 0) {
+                    int depth = 1;
+                    while (depth > 0) {
+                        bf--;
+                        if (*bf == ']') {
+                            depth++;
+                        } else if (*bf == '[') {
+                            depth--;
+                        }
+                    }
+                }
+                break;
+        }
+        bf++;
+    }
+
+    return output;
+}
+
+int main(int argc, char** argv)
+{
     if (argc < 2) {
         printf("Usage: %s <file>", argv[0]);
         return 1;
